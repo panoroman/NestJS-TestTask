6ad43ca3b632480bc10a6aa746017fb02ab3060f
Roman <you@example.com>
2021-01-14 18:05:20 +0700

    Add friends

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupsModule } from './groups/groups.module';
+import { FriendsModule } from './friends/friends.module';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(), GroupsModule],
+  imports: [UsersModule, TypeOrmModule.forRoot(), GroupsModule, FriendsModule],
   controllers: [AppController],
   providers: [AppService],
 })
